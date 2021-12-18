b9fb722b98b32d80d6e83d48208c748a4692b90a
craig <craig@notebook>
2021-12-18 08:10:15 -0500

    feedback

--- extension.js
+++ extension.js
@@ -1,100 +1,88 @@
 'use strict';
 
+const ExtensionUtils = imports.misc.extensionUtils;
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
 const Main = imports.ui.main;
 const St = imports.gi.St;
-const Lang = imports.lang;
 const Soup = imports.gi.Soup;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
 const PanelMenu = imports.ui.panelMenu;
 
 let serverStatus;
-let session = new Soup.SessionAsync();
+let session;
 let icon;
 let serverIcon;
 let serverUpIcon;
 let serverDownIcon;
 let intervalID;
-let clearInterval = GLib.source_remove;
+let clearInterval;
 let settings;
 
 function init() {
-	log(`Initializing ${Extension.metadata.name} version ${Extension.metadata.version}`);
 }
 
-// Object prototype
-const ServerStatus = GObject.registerClass(
-	class ServerStatus extends PanelMenu.Button {
-		_init() {
-			super._init(0);
-			
-			let GioSSS = Gio.SettingsSchemaSource;
-            let schemaSource = GioSSS.new_from_directory(
-                Extension.dir.get_child("schemas").get_path(),
-                GioSSS.get_default(),
-                false
-            );
-            
-            let schemaObj = schemaSource.lookup('org.gnome.shell.extensions.serverstatus', true);
-            if (!schemaObj) {
-                throw new Error('cannot find schemas');
-            }
-            settings = new Gio.Settings({ settings_schema : schemaObj });
-			
-			let path = Extension.dir.get_path();
-			serverIcon = Gio.icon_new_for_string(path + '/server.svg');
-			serverUpIcon = Gio.icon_new_for_string(path + '/server-up.svg');
-			serverDownIcon = Gio.icon_new_for_string(path + '/server-down.svg');
+const ServerStatus = GObject.registerClass({
+	GTypeName: 'ServerStatus',
+}, class ServerStatus extends PanelMenu.Button {
+	_init() {
+		super._init(0);
+		
+		settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.serverstatus');
+		
+		let path = Extension.dir.get_path();
+		serverIcon = Gio.icon_new_for_string(path + '/server.svg');
+		serverUpIcon = Gio.icon_new_for_string(path + '/server-up.svg');
+		serverDownIcon = Gio.icon_new_for_string(path + '/server-down.svg');
 
-			// panel button
-			icon = new St.Icon({
-				gicon: serverIcon,
-				style_class: 'system-status-icon',
-			});
-			this.add_child(icon);			
-		}
-	});
+		icon = new St.Icon({
+			gicon: serverIcon,
+			style_class: 'system-status-icon',
+		});
+		this.add_child(icon);			
+	}
+});
 
 function enable() {
 	serverStatus = new ServerStatus();
 	Main.panel.addToStatusArea('Server Status', serverStatus, 1);
+	session = new Soup.SessionAsync();
+	clearInterval = GLib.source_remove;
 	intervalID = setInterval(() => update(getURL()), 10000);
 }
 
 function disable() {
-	log(`Disabling ${Extension.metadata.name} version ${Extension.metadata.version}`);
 	clearInterval(intervalID)
 	if (serverStatus !== null) {
 		serverStatus.destroy();
 	}
+	serverIcon = null;
+    serverUpIcon = null;
+    serverDownIcon = null;
 }
 
 function update(url) {
     get(url, function(status_code, body) {
         icon.gicon = (status_code == 200) ? serverUpIcon : serverDownIcon;
-        Mainloop.quit(true); 
+        return GLib.SOURCE_REMOVE; 
     });
-    Mainloop.run(true);
+    return GLib.SOURCE_CONTINUE;
 }
 
 function get(url, callback) { 
     let request = Soup.Message.new('GET', url);
-    session.queue_message(request, Lang.bind(this, function(session, message) {
+    
+    session.queue_message(request, (session, message) => {
         callback(message.status_code, request.response_body.data);
-    }));
+    });
 }
 
-function setInterval(func, delay, ...args) {
-    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, () => {
-        func(...args);
-        return GLib.SOURCE_CONTINUE;
-    });
+function setInterval(func, delay) {
+    return GLib.timeout_add(GLib.PRIORITY_DEFAULT, delay, func);
 }
 
 function getURL(){
     return settings.get_string('url');
 }
-
--- prefs.js
+++ prefs.js
@@ -3,17 +3,11 @@
 const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
-
-// It's common practice to keep GNOME API and JS imports in separate blocks
 const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
 
 // Like `extension.js` this is used for any one-time setup like translations.
 function init() {
-    log(`initializing ${Me.metadata.name} Preferences`);
 }
-
 
 // This function is called when the preferences window is first created to build
 // and return a Gtk widget. As an example we'll create and return a GtkLabel.
@@ -22,17 +16,6 @@ function buildPrefsWidget() {
     this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.serverstatus');
 
     let grid = new Gtk.Grid();
-
-    // At the time buildPrefsWidget() is called, the window is not yet prepared
-    // so if you want to access the headerbar you need to use a small trick
-    GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
-        // In GTK4 (GNOME 40), call `get_root()` instead of `get_toplevel()`
-        let window = grid.get_toplevel();
-        let headerBar = window.get_titlebar();
-        headerBar.title = `${Me.metadata.name} Preferences`;
-        
-        return GLib.SOURCE_REMOVE;
-    });
     
     let label = new Gtk.Label({
         label: "   Enter an URL to poll:  ",
@@ -51,4 +34,3 @@ function buildPrefsWidget() {
 
     return grid;
 }
-
