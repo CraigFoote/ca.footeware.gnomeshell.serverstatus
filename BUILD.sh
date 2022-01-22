3dcac22519b7dd3b0adde7d6a66971c5a948e5c3
CraigFoote <CraigFoote@gmail.com>
2022-01-22 10:32:48 -0500

    add missing prefs.js

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-git archive -o serverstatus@footeware.ca.zip HEAD -- extension.js metadata.json LICENSE README.md screenshot.png server.svg server-bad.svg server-down.svg server-up.svg icon.png schemas
+git archive -o serverstatus@footeware.ca.zip HEAD -- extension.js prefs.js metadata.json LICENSE README.md screenshot.png server.svg server-bad.svg server-down.svg server-up.svg icon.png schemas
